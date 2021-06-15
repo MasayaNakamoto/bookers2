8d28e043bea0abd9dc60695a2f959318a07e2f61
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-15 11:00:23 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  root to: 'homes#top'
+   resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+   resources :users, only: [:index, :show, :edit, :update]
+   post 'books/new'
+   get 'homes/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
