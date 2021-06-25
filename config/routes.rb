d8a7cca39377b7c63b8724bf849cb26c45266f54
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-25 10:16:57 +0000

    kakunin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
    resources :books, only: [:new, :create, :index, :show, :destroy, :update, :edit]
    resources :users, only: [:index, :show, :edit, :update, :create]
-#   get 'books/:id' => 'books#show', as: 'books'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
