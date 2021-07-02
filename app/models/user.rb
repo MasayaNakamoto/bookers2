46414ab6233a45ee9c4deeaac2999b62f5fd0518
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-02 10:56:00 +0000

    muzukashii

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,33 @@ class User < ApplicationRecord
   has_many :xxx, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :xxx, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # ここから下↓はもしかしたらいらんかも↓
-    def follow(user_id)
-      relationships.create(followed_id: user_id)
-    end
-    def unfollow(user_id)
-      relationships.find_by(followed_id: user_id).destroy
-    end
-    def following?(user)
-      followings.include?(user)
-    end
+    # def follow(user_id)
+    #   relationships.create(followed_id: user_id)
+    # end
+
+    # def unfollow(user_id)
+    #   relationships.find_by(followed_id: user_id).destroy
+    # end
+
+    # def following?(user)
+    #   followings.include?(user)
+    # end
+
+    # ユーザーをフォローする
+def follow(user_id)
+  follower.create(followed_id: user_id)
+end
+
+# ユーザーのフォローを外す
+def unfollow(user_id)
+  follower.find_by(followed_id: user_id).destroy
+end
+
+# フォローしていればtrueを返す
+def following?(user)
+  following_user.include?(user)
+end
+
     # ここまでいらんやつかも
 
   attachment :profile_image
