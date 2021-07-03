d310e7c0494195c5935b1bb339ca6c56ff757321
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-03 06:49:25 +0000

    ouyoukadai4finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,37 +7,42 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  has_many :xxx, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :xxx, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+ has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+ has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+
+ has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 【class_name: "Relationship"】は省略可能
+ has_many :followings, through: :relationships, source: :followed
   # ここから下↓はもしかしたらいらんかも↓
-    # def follow(user_id)
-    #   relationships.create(followed_id: user_id)
-    # end
+    def follow(user_id)
+      relationships.create(followed_id: user_id)
+    end
 
-    # def unfollow(user_id)
-    #   relationships.find_by(followed_id: user_id).destroy
-    # end
+    def unfollow(user_id)
+      relationships.find_by(followed_id: user_id).destroy
+    end
 
-    # def following?(user)
-    #   followings.include?(user)
-    # end
+    def following?(user)
+      followings.include?(user)
+    end
 
+# ↓これ多分使わんから消していい
     # ユーザーをフォローする
-def follow(user_id)
-  follower.create(followed_id: user_id)
-end
+# def follow(user_id)
+#   follower.create(followed_id: user_id)
+# end
 
-# ユーザーのフォローを外す
-def unfollow(user_id)
-  follower.find_by(followed_id: user_id).destroy
-end
+# # ユーザーのフォローを外す
+# def unfollow(user_id)
+#   follower.find_by(followed_id: user_id).destroy
+# end
 
 # フォローしていればtrueを返す
-def following?(user)
-  following_user.include?(user)
-end
-
-    # ここまでいらんやつかも
+# def following?(user)
+#   followings.include?(user)
+# end
+ # ここまでいらんやつかも
 
   attachment :profile_image
 
