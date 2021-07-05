6008e365397098685e60ce673c4372477977dc8f
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-05 10:58:36 +0000

    6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,13 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book .find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @book = Book .find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @book_comment = current_user.book_comments.new
+    # binding.pry
+
+    @comment.save
+    # binding.pry
     # redirect_to book_path(book)
   end
 
